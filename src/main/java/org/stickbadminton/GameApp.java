93ca7f6b6479154ff3f3f60aff2bbc9274bfd77a
SfumatoCannon <3075964583@qq.com>
2025-09-10 15:48:03 +0800

    窗口大小可变、可最大化

--- src/main/java/org/stickbadminton/GameApp.java
+++ src/main/java/org/stickbadminton/GameApp.java
@@ -2,12 +2,18 @@ package org.stickbadminton;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.scene.GameScene;
 import com.almasb.fxgl.dsl.FXGL;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
+
 import org.stickbadminton.gamecomponent.Room1;
+
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameScene;
 
 public class GameApp extends GameApplication {
 
@@ -15,7 +21,9 @@ public class GameApp extends GameApplication {
     protected void initSettings(GameSettings settings) {
         settings.setWidth(900);
         settings.setHeight(600);
+        settings.setManualResizeEnabled(true); // 可拉伸窗口大小
         settings.setScaleAffectedOnResize(true);
+        settings.setPreserveResizeRatio(true);
         settings.setTitle("火柴人打羽毛球");
         settings.setVersion("v0.1");
         settings.setTicksPerSecond(60);
