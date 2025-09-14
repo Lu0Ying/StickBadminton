2ed8142271ebfa6cb67c8a0959968b90dde23d32
苑博祥 <2940210094@qq.com>
2025-09-14 15:24:31 +0800

    完成难度调整

--- src/main/java/org/stickbadminton/gamecomponent/ComputerDecision.java
+++ src/main/java/org/stickbadminton/gamecomponent/ComputerDecision.java
@@ -93,12 +93,10 @@ public class ComputerDecision {
             this.difficultyLevel = 1.0;
         if(random.nextDouble()>difficultyLevel*0.1)
         {
-            System.out.println("random");
             this.racketRadius = GameProperties.racketRadius*difficultyLevel;
             this.hitAreaRadius = GameProperties.hitAreaRadius*difficultyLevel;
         }
         else{
-            System.out.println("666");
         this.racketRadius = GameProperties.racketRadius;
         this.hitAreaRadius = GameProperties.hitAreaRadius;
     }
